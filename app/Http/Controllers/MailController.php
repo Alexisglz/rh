d66f1a21f66cdbc02f170e9b5948e0c8d8bca289
Rodolfo <rodorogz@gmail.com>
2019-08-21 13:20:58 -0500

    validar correo

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -13,18 +13,22 @@ use Session;
 class MailController extends Controller
 {
     public function emailIncore(Request $request){
-        $body              = $request->body;
-        $subject           = $request->subject;
-        $from              = $request->from;
-        $from_name         = $request->from_name;
-        $dest              = $request->dest;
-        $dest_copia        = $request->dest_copia;
-        $dest_copia_oculta = $request->dest_copia_oculta;
-        $url_adjunto       = $request->url_adjunto;
-        //dd($dest);
-        foreach ($dest as $item){
-            Mail::to($item)->cc($dest_copia)->bcc($dest_copia_oculta)->send(new IncoreMail($body,$subject));
+        try{
+            $body              = $request->body;
+            $subject           = $request->subject;
+            $from              = $request->from;
+            $from_name         = $request->from_name;
+            $dest              = $request->dest;
+            $dest_copia        = $request->dest_copia;
+            $dest_copia_oculta = $request->dest_copia_oculta;
+            $url_adjunto       = $request->url_adjunto;
+            //dd($dest);
+            foreach ($dest as $item){
+                Mail::to($item)->cc($dest_copia)->bcc($dest_copia_oculta)->send(new IncoreMail($body,$subject));
+            }
+            return response()->json(true);
+        }catch (\Exception $e){
+            return response()->json(false);
         }
-        return response()->json(true);
     }
 }

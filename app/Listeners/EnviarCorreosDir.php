25429577c52e80c6e51319c8a197c501efc42b3a
Rodolfo <rodorogz@gmail.com>
2019-08-26 11:26:20 -0500

    ajuste

--- app/Listeners/EnviarCorreosDir.php
+++ app/Listeners/EnviarCorreosDir.php
@@ -10,7 +10,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Mail;
 
-class EnviarCorreosDir implements ShouldQueue
+class EnviarCorreosDir
 {
     /**
      * @var false|string

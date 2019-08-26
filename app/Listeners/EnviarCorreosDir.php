19ceac49498f2dba807641648bce46cc73459069
Rodolfo <rodorogz@gmail.com>
2019-08-26 11:23:22 -0500

    debug

--- app/Listeners/EnviarCorreosDir.php
+++ app/Listeners/EnviarCorreosDir.php
@@ -35,31 +35,35 @@ class EnviarCorreosDir implements ShouldQueue
      */
     public function handle(IncidenciasNotificar $event)
     {
-        $tipo    = $event->tipo;
-        $perms   = $event->perms;
-        $periodo = IncidenciaPeriodo::where('fecha_inicio','<=', $this->date)
-            ->where('fecha_fin','>=', $this->date)->first();
-        $fecha_inicio = $periodo->fecha_inicio;
-        $fecha_fin    = $periodo->fecha_fin;
-        $fecha_envio  = $periodo->fecha_envio;
-        $email        = config('app.mail_dev');
-        $oculto       = config('app.mail_dev');
-        switch ($tipo){
-            case 'auth':
-                $correos = DB::table('vista_permisos_empleados')
-                    ->whereIn('codigo', $perms)
-                    ->groupBy('id_usuario')
-                    ->get();
-                if (config('app.env')=="local")
-                    Mail::to($email)->bcc($oculto)->send(new IncidenciasAutorizar('/auth',$fecha_envio,$correos->toArray(),$periodo->periodo_nombre));
-                if (config('app.env')=="production") {
-                    foreach ($correos  as $correo){
-                        Mail::to($correo->correo)->bcc($oculto)->send(new IncidenciasAutorizar('/auth',$fecha_envio,$correos->toArray(),$periodo->periodo_nombre));
+        try{
+            $tipo    = $event->tipo;
+            $perms   = $event->perms;
+            $periodo = IncidenciaPeriodo::where('fecha_inicio','<=', $this->date)
+                ->where('fecha_fin','>=', $this->date)->first();
+            $fecha_inicio = $periodo->fecha_inicio;
+            $fecha_fin    = $periodo->fecha_fin;
+            $fecha_envio  = $periodo->fecha_envio;
+            $email        = config('app.mail_dev');
+            $oculto       = config('app.mail_dev');
+            switch ($tipo){
+                case 'auth':
+                    $correos = DB::table('vista_permisos_empleados')
+                        ->whereIn('codigo', $perms)
+                        ->groupBy('id_usuario')
+                        ->get();
+                    if (config('app.env')=="local")
+                        Mail::to($email)->bcc($oculto)->send(new IncidenciasAutorizar('/auth',$fecha_envio,$correos->toArray(),$periodo->periodo_nombre));
+                    if (config('app.env')=="production") {
+                        foreach ($correos  as $correo){
+                            Mail::to($correo->correo)->bcc($oculto)->send(new IncidenciasAutorizar('/auth',$fecha_envio,$correos->toArray(),$periodo->periodo_nombre));
+                        }
                     }
-                }
-                break;
-            case 'envio':
-                break;
+                    break;
+                case 'envio':
+                    break;
+            }
+        }catch (\Exception $e){
+            echo $e;
         }
     }
 }

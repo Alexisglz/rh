d19fac0316c972ca0b8c6aecbc6dc6e64809758e
Rodolfo <rodorogz@gmail.com>
2019-09-24 11:50:15 -0500

    tags

--- app/Models/SolicitudesBitacoraBajas.php
+++ app/Models/SolicitudesBitacoraBajas.php
@@ -4,6 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\SolicitudesBitacoraBajas
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\SolicitudesBitacoraBajas newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\SolicitudesBitacoraBajas newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\SolicitudesBitacoraBajas query()
+ * @mixin \Eloquent
+ */
 class SolicitudesBitacoraBajas extends Model
 {
   protected $table = 'solicitudes_bitacora_bajas';
